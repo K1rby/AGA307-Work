f5ef2ac84c9803a6af980266a147b9ae19cfea03
Kaitlyn <diabolopersona@gmail.com>
2021-10-13 14:24:48 +1100

    Week 3

---
+++ AGA307-Programming-Assessment-1-main/Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public Transform[] spawnPoints;
+    public GameObject[] enemyTypes;
+    public List<GameObject> enemies;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            Debug.Log((i = 1).ToString());
+        }
+
+        SpawnEnemy();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    [ContextMenu("Spawn Enemy")]
+    public void SpawnEnemy()
+    {
+        for (int i = 0; i < enemyTypes.Length; i++)
+        {
+            GameObject newSpawn = Instantiate(enemyTypes[i], spawnPoints[i].position, spawnPoints[i].rotation);
+            enemies.Add(newSpawn);
+            Debug.Log(enemies.Count);
+        }
+
+        Debug.Log("Total:" + enemies.Count + "enemies");
+    }
+}
